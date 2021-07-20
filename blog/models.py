dde706e8f93465904ffcfe428ce1a082c7146353
Kagiyama <sabitsukimaid@gmail.com>
2021-07-20 08:26:14 -0700

    Added a view for all posts

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
@@ -8,7 +9,12 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
-    date = models.DateTimeField()
- 
+    date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.date = timezone.now()
+        self.save()
+
+
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request, 'blog/posts.html', {'posts': posts})
